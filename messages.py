cbc9c622bb139269a24861dcf988a51e8c2a08ad
vogronicspatrik <vogronicspatrik@gmail.com>
2016-08-03 11:08:07 +0200

    messages by Patrik

---
+++ messages.py
@@ -0,0 +1,23 @@
+
+
+
+greetings = '''Dear %s! Thank you for your applicaton to Codecool.Your application code is %s, the closest city,
+where you can start to learn coding is %s.
+
+See you soon!
+
+Cheers!
+Mentors
+'''
+
+information_for_applicant = ''' Dear %s! You will have an interview at %s where your mentors will be %s!
+
+See you soon!
+
+Cheers!
+Mentors'''
+
+information_for_mentor = '''Dear %s! You will have an interview at %s with our new applicant, who's name is %s!
+
+Cheers!
+CodecoolRobot!'''
