92053469172c8883b8e6ea453cd9b1b2a3b6ec1c
TBalint19 <toth910719balint@gmail.com>
2016-08-04 11:34:42 +0200

    debugging data creating

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 from models import *
 
 db.connect()
-# db.drop_tables([School, City, Applicant, Mentor, InterviewSlot, SlotMentor, Interview, QuestionAnswer], safe=True)
+db.drop_tables([School, City, Applicant, Mentor, InterviewSlot, SlotMentor, Interview, QuestionAnswer], safe=True)
 db.create_tables([School, City, Applicant, Mentor, InterviewSlot, SlotMentor, Interview, QuestionAnswer], safe=True)
