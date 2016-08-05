b3acdfde7825efc9054a400c0ab9a01a7b722f97
CsibiJani <jozsef.berko.ifj@gmail.com>
2016-08-05 12:40:39 +0200

    config.py basic state

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
-fromemail = 'codecoolrobot@gmail.com'
-password = 'codecoolrobot1'
+fromemail = 'youremailhere'
+password = 'youremailpasswordhere'
 smtp = 'smtp.gmail.com'
-toemail = 'codecoolrobot@gmail.com' # without the domain name. example:john.smith@gmail.com ----> john.smith
-dbname = 'csibi'
-username = 'csibi' # for the database connection
+toemail = 'receivingemailhere'  #  without the domain name. example:john.smith@gmail.com ----> john.smith
+dbname = 'dbname'
+username = 'username'#  for the database connection
 
