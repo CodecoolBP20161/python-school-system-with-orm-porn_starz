1bb9004e8118c6a9dfdf1ee378118e26abcbb4cf
CsibiJani <jozsef.berko.ifj@gmail.com>
2016-08-05 10:28:47 +0200

    initialized config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+fromemail = 'youremailaddresshere'
+password = 'yourpasswordhere'
+smtp = 'your smtp address here'
+toemail = 'receiveremailaddresshere' # without the domain name. example:john.smith@gmail.com ----> john.smith
+dbname = 'yourdatabasename'
+username = 'yourlocalusername' # for the database connection
+
