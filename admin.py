1ba0bd9b9e765d1273c645745ee95ac40c2b6320
CsibiJani <jozsef.berko.ifj@gmail.com>
2016-08-05 10:00:56 +0200

    fixed syntax

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@ from admin_interface import AdminInterface
 
 
 def admin_interface():
-logged_in_as_admin = AdminInterface.log_in()
-if logged_in_as_admin:
-    interface = AdminInterface()
-    interface.run()
+    logged_in_as_admin = AdminInterface.log_in()
+    if logged_in_as_admin:
+        interface = AdminInterface()
+        interface.run()
